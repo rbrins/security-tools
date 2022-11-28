0c1fb92e34f3071315d5811e8e10947b73c1ab87
Russell <russbrins4@gmail.com>
2022-11-28 14:38:27 -0500

    Added ping-sweep.sh

---
+++ ping-sweep.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# just debugging values, comment or delete out
+echo "First 3 Octets:                  $1"
+echo "Starting value last Octet:       $2"
+echo "Ending value last Octet:         $3"
+
+for i in $(seq $2 $3)
+do
+    IPADDR="$1" + "." + "$i"
+    OUTPUT=$(ping -c 1 $IPADDR)
+    FOUND=$(echo $OUTPUT | grep -i "bytes from")
+    
+    if [ ! -z $FOUND ]
+    then
+	echo "$IPADDR"
+    fi
+done
+
