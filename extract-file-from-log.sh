18065c377afbe7649905121a0b1497bc1852f098
Russell <russbrins4@gmail.com>
2022-11-28 21:53:39 -0500

    Updated regex, default file type value, and FOUND command

--- extract-file-from-log.sh
+++ extract-file-from-log.sh
@@ -3,7 +3,7 @@
 # extract file types from a log
 
 # Default values and debugging info
-FILEEXT=${2:-".js"}
+FILEEXT=${2:-"js"}
 TMPFILELOC=${3:-"/tmp/extractingdump.txt"}
 echo "Log Location:            $1"
 echo "Default File ext:        $FILEEXT"
@@ -11,11 +11,12 @@ echo "Temporary File Location: $TMPFILELOC"
 
 # with line anchors ^ and $ (escaped with \) 
 # Just looking at file that start with forward slash (may need to add or other special characters)
-REGEX="^/.*$FILEEXT\$"
+REGEX="/.\.*$FILEEXT"
 
 cat $1 | while read line
 do
-    FOUND=$( echo $line | grep -e $REGEX )
+    # double rev is like reverse reverse in code thanks stackoverflow https://stackoverflow.com/questions/22727107/how-to-find-the-last-field-using-cut for the idea
+    FOUND=$( echo $line | grep -oe $REGEX | rev | cut -d'/' -f1 | rev )
 
     if [ ! -z "$FOUND" ]
     then
